e713c6a663e4031b79b93070a5ccbbe2845e9d8e
oplengardeza <oplengardeza@gmail.com>
2022-11-04 22:08:22 +0800

    FIXINGS

--- src/pages/dashboardRoutes/singleViews/ShopSingleView.js
+++ src/pages/dashboardRoutes/singleViews/ShopSingleView.js
@@ -1,10 +1,13 @@
-import { Typography, Box, Grid, Avatar } from "@mui/material";
+import { Typography, Box, Grid, Avatar, IconButton } from "@mui/material";
 import { doc, onSnapshot } from "firebase/firestore";
 import moment from "moment";
 import React, { useEffect, useState } from "react";
 import { db } from "../../../utils/firebase";
-function ShopSingleView({ data, shopID, ownerID }) {
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useNavigate } from "react-router-dom";
 
+function ShopSingleView({ data, shopID, ownerID }) {
+    const navigate = useNavigate()
     const [shopData, setShopData] = useState({});
 
     useEffect(() => {
@@ -14,127 +17,52 @@ function ShopSingleView({ data, shopID, ownerID }) {
         return unsub;
     }, [shopID, ownerID])
 
-    console.log(shopData)
+    const onBack = () => {
+        navigate("/admin/users-info")
+    }
+
     return (
-        <Box sx={{
-            padding: 5
-        }}
-            container component={Grid} justifyContent="center"
-        >
-            <Box sx={{ display: 'flex', flexDirection: 'column', maxWidth: 600 }}>
-                <Box container component={Grid} justifyContent="center">
-                    <Typography sx={{
-                        fontSize: 40, 
-                        fontWeight: 'bold',
-                        letterSpacing: 1
-                    }}> Shop Details</Typography>
-                </Box>
-                <Box container component={Grid} justifyContent="center" sx={{
-                    marginTop: 3
-                }}>
-                    <Avatar sx={{ height: 300, width: 250 }} variant="square" src={shopData.imageShop} />
-                </Box>
-                <Box container component={Grid} justifyContent="center" sx={{
-                    marginTop: 2
-                }}>
-                    <Typography sx={{
-                        fontSize: 18,
-                        fontWeight: 'bold',
-                        letterSpacing: 1
-                    }}>
-                        {shopData.businessName}
-                    </Typography>
-                </Box>
-                <Box container component={Grid} justifyContent="space-between" sx={{
-                    marginTop: 3,
-                    width: 500,
-                    boxShadow: 2,
-                    padding: 4,
-                    borderRadius: 2,
-                    backgroundColor: '#F0F0F3'
-                }}>
-                    <Box>
+        <Box>
+            <IconButton sx={{ alignSelf: 'flex-start' }} onClick={onBack}>
+                <ArrowBackIcon sx={{ fontSize: 40 }} />
+            </IconButton>
+            <Box sx={{
+                padding: 5
+            }}
+                container component={Grid} justifyContent="center"
+            >
+                <Box sx={{ display: 'flex', flexDirection: 'column', maxWidth: 600 }}>
+
+                    <Box container component={Grid} justifyContent="center">
                         <Typography sx={{
-                            fontSize: 18,
+                            fontSize: 40,
                             fontWeight: 'bold',
                             letterSpacing: 1
-                        }}>
-                            Shop ID
-                        </Typography>
-                        <Typography sx={{
-                            fontSize: 18,
-                            fontWeight: 'bold',
-                            letterSpacing: 1
-                        }}>
-                            {shopID.substring(0, 7)}
-                        </Typography>
+                        }}> Shop Details</Typography>
                     </Box>
-                    <Box>
-                        <Typography sx={{
-                            fontSize: 18,
-                            fontWeight: 'bold',
-                            letterSpacing: 1
-                        }}>
-                            Seller ID
-                        </Typography>
-                        <Typography sx={{
-                            fontSize: 18,
-                            fontWeight: 'bold',
-                            letterSpacing: 1
-                        }}>
-                            {ownerID.substring(0, 7)}
-                        </Typography>
-                    </Box>
-                </Box>
-                <Box container component={Grid} 
-                sx={{
-                    marginTop: 3,
-                    width: 500,
-                    boxShadow: 2,
-                    padding: 4,
-                    borderRadius: 2,
-                    backgroundColor: '#F0F0F3'
-                }}>
-                    <Box container component={Grid} justifyContent="space-between" sx={{
-                        marginTop: 3,
-                        width: 500
+                    <Box container component={Grid} justifyContent="center" sx={{
+                        marginTop: 3
                     }}>
-                        <Box>
-                            <Typography sx={{
-                                fontSize: 18,
-                                fontWeight: 'bold',
-                                letterSpacing: 1
-                            }}>
-                                Owner Name
-                            </Typography>
-                            <Typography sx={{
-                                fontSize: 18,
-                                fontWeight: 'bold',
-                                letterSpacing: 1
-                            }}>
-                                {shopData.fullName}
-                            </Typography>
-                        </Box>
-                        <Box>
-                            <Typography sx={{
-                                fontSize: 18,
-                                fontWeight: 'bold',
-                                letterSpacing: 1
-                            }}>
-                                Shop Location
-                            </Typography>
-                            <Typography sx={{
-                                fontSize: 18,
-                                fontWeight: 'bold',
-                                letterSpacing: 1
-                            }}>
-                                {shopData.address}
-                            </Typography>
-                        </Box>
+                        <Avatar sx={{ height: 300, width: 250 }} variant="square" src={shopData.imageShop} />
+                    </Box>
+                    <Box container component={Grid} justifyContent="center" sx={{
+                        marginTop: 2
+                    }}>
+                        <Typography sx={{
+                            fontSize: 18,
+                            fontWeight: 'bold',
+                            letterSpacing: 1
+                        }}>
+                            {shopData.businessName}
+                        </Typography>
                     </Box>
                     <Box container component={Grid} justifyContent="space-between" sx={{
                         marginTop: 3,
-                        width: 500
+                        width: 500,
+                        boxShadow: 2,
+                        padding: 4,
+                        borderRadius: 2,
+                        backgroundColor: '#F0F0F3'
                     }}>
                         <Box>
                             <Typography sx={{
@@ -142,14 +70,14 @@ function ShopSingleView({ data, shopID, ownerID }) {
                                 fontWeight: 'bold',
                                 letterSpacing: 1
                             }}>
-                                Date Created
+                                Shop ID
                             </Typography>
                             <Typography sx={{
                                 fontSize: 18,
                                 fontWeight: 'bold',
                                 letterSpacing: 1
                             }}>
-                                {moment(shopData.dateCreated).format("ll")}
+                                {shopID.substring(0, 7)}
                             </Typography>
                         </Box>
                         <Box>
@@ -158,53 +86,137 @@ function ShopSingleView({ data, shopID, ownerID }) {
                                 fontWeight: 'bold',
                                 letterSpacing: 1
                             }}>
-                                Shop Contact
+                                Seller ID
                             </Typography>
                             <Typography sx={{
                                 fontSize: 18,
                                 fontWeight: 'bold',
                                 letterSpacing: 1
                             }}>
-                                {shopData.contactNo}
+                                {ownerID.substring(0, 7)}
                             </Typography>
                         </Box>
                     </Box>
-                    <Box container component={Grid} justifyContent="space-between" sx={{
-                        marginTop: 3,
-                        width: 500
-                    }}>
-                        <Box>
-                            <Typography sx={{
-                                fontSize: 18,
-                                fontWeight: 'bold',
-                                letterSpacing: 1
-                            }}>
-                                Seller Contact
-                            </Typography>
-                            <Typography sx={{
-                                fontSize: 18,
-                                fontWeight: 'bold',
-                                letterSpacing: 1
-                            }}>
-                                {shopData.phone}
-                            </Typography>
+                    <Box container component={Grid}
+                        sx={{
+                            marginTop: 3,
+                            width: 500,
+                            boxShadow: 2,
+                            padding: 4,
+                            borderRadius: 2,
+                            backgroundColor: '#F0F0F3'
+                        }}>
+                        <Box container component={Grid} justifyContent="space-between" sx={{
+                            marginTop: 3,
+                            width: 500
+                        }}>
+                            <Box>
+                                <Typography sx={{
+                                    fontSize: 18,
+                                    fontWeight: 'bold',
+                                    letterSpacing: 1
+                                }}>
+                                    Owner Name
+                                </Typography>
+                                <Typography sx={{
+                                    fontSize: 18,
+                                    fontWeight: 'bold',
+                                    letterSpacing: 1
+                                }}>
+                                    {shopData.fullName}
+                                </Typography>
+                            </Box>
+                            <Box>
+                                <Typography sx={{
+                                    fontSize: 18,
+                                    fontWeight: 'bold',
+                                    letterSpacing: 1
+                                }}>
+                                    Shop Location
+                                </Typography>
+                                <Typography sx={{
+                                    fontSize: 18,
+                                    fontWeight: 'bold',
+                                    letterSpacing: 1
+                                }}>
+                                    {shopData.address}
+                                </Typography>
+                            </Box>
                         </Box>
-                        <Box>
-                            <Typography sx={{
-                                fontSize: 18,
-                                fontWeight: 'bold',
-                                letterSpacing: 1
-                            }}>
-                                Shop Verified
-                            </Typography>
-                            <Typography sx={{
-                                fontSize: 18,
-                                fontWeight: 'bold',
-                                letterSpacing: 1,
-                                color: shopData.isShopVerified === true ? "green" : "red"
-                            }}>
-                                {shopData.isShopVerified === true ? "Yes" : "No"}
-                            </Typography>
+                        <Box container component={Grid} justifyContent="space-between" sx={{
+                            marginTop: 3,
+                            width: 500
+                        }}>
+                            <Box>
+                                <Typography sx={{
+                                    fontSize: 18,
+                                    fontWeight: 'bold',
+                                    letterSpacing: 1
+                                }}>
+                                    Date Created
+                                </Typography>
+                                <Typography sx={{
+                                    fontSize: 18,
+                                    fontWeight: 'bold',
+                                    letterSpacing: 1
+                                }}>
+                                    {moment(shopData.dateCreated).format("ll")}
+                                </Typography>
+                            </Box>
+                            <Box>
+                                <Typography sx={{
+                                    fontSize: 18,
+                                    fontWeight: 'bold',
+                                    letterSpacing: 1
+                                }}>
+                                    Shop Contact
+                                </Typography>
+                                <Typography sx={{
+                                    fontSize: 18,
+                                    fontWeight: 'bold',
+                                    letterSpacing: 1
+                                }}>
+                                    {shopData.contactNo}
+                                </Typography>
+                            </Box>
+                        </Box>
+                        <Box container component={Grid} justifyContent="space-between" sx={{
+                            marginTop: 3,
+                            width: 500
+                        }}>
+                            <Box>
+                                <Typography sx={{
+                                    fontSize: 18,
+                                    fontWeight: 'bold',
+                                    letterSpacing: 1
+                                }}>
+                                    Seller Contact
+                                </Typography>
+                                <Typography sx={{
+                                    fontSize: 18,
+                                    fontWeight: 'bold',
+                                    letterSpacing: 1
+                                }}>
+                                    {shopData.phone}
+                                </Typography>
+                            </Box>
+                            <Box>
+                                <Typography sx={{
+                                    fontSize: 18,
+                                    fontWeight: 'bold',
+                                    letterSpacing: 1
+                                }}>
+                                    Shop Verified
+                                </Typography>
+                                <Typography sx={{
+                                    fontSize: 18,
+                                    fontWeight: 'bold',
+                                    letterSpacing: 1,
+                                    color: shopData.isShopVerified === true ? "green" : "red"
+                                }}>
+                                    {shopData.isShopVerified === true ? "Yes" : "No"}
+                                </Typography>
+                            </Box>
                         </Box>
                     </Box>
                 </Box>
--- src/pages/dashboardRoutes/singleViews/SingleView.js
+++ src/pages/dashboardRoutes/singleViews/SingleView.js
@@ -1,105 +1,115 @@
-import { Typography, Box, Grid, Avatar } from "@mui/material";
+import { Typography, Box, Grid, Avatar, IconButton } from "@mui/material";
 import React from "react";
 import { deepOrange } from "@mui/material/colors";
-
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 function SingleView({ data }) {
+
+  const onBack = () => {
+    window.location.reload()
+  }
+
   return (
-    <Box sx={{
-      padding: 10
-    }}
-      container component={Grid} justifyContent="center"
-    >
-      <Box sx={{ display: 'flex', flexDirection: 'column', maxWidth: 600 }}>
-        <Box container component={Grid} justifyContent="center">
-          {data.photoURL === "" ?
-            <Avatar sx={{ bgcolor: deepOrange[500], height: 130, width: 130, fontSize: 40, fontWeight: 'bold', boxShadow: 2 }}>{data.fullname.substring(0, 1)}</Avatar> :
-            <Avatar src={data.photoURL} sx={{ height: 150, width: 150, boxShadow: 2 }} />
-          }
-        </Box>
-        <Box container component={Grid} justifyContent="center" sx={{
-          marginTop: 2
-        }}>
-          <Typography sx={{
-            fontSize: 18,
-            fontWeight: 'bold',
-            letterSpacing: 1
+    <Box>
+      <IconButton sx={{ alignSelf: 'flex-start' }} onClick={onBack}>
+        <ArrowBackIcon sx={{ fontSize: 40 }} />
+      </IconButton>
+      <Box sx={{
+        padding: 10
+      }}
+        container component={Grid} justifyContent="center"
+      >
+        <Box sx={{ display: 'flex', flexDirection: 'column', maxWidth: 600 }}>
+          <Box container component={Grid} justifyContent="center">
+            {data.photoURL === "" ?
+              <Avatar sx={{ bgcolor: deepOrange[500], height: 130, width: 130, fontSize: 40, fontWeight: 'bold', boxShadow: 2 }}>{data.fullname.substring(0, 1)}</Avatar> :
+              <Avatar src={data.photoURL} sx={{ height: 150, width: 150, boxShadow: 2 }} />
+            }
+          </Box>
+          <Box container component={Grid} justifyContent="center" sx={{
+            marginTop: 2
           }}>
-            {data.fullname}
-          </Typography>
-        </Box>
-        <Box container component={Grid} justifyContent="space-between" sx={{
-          marginTop: 3,
-          width: 500
-        }}>
-          <Box>
             <Typography sx={{
               fontSize: 18,
               fontWeight: 'bold',
               letterSpacing: 1
             }}>
-              Email
-            </Typography>
-            <Typography sx={{
-              fontSize: 18,
-              fontWeight: 'bold',
-              letterSpacing: 1
-            }}>
-              {data.email}
+              {data.fullname}
             </Typography>
           </Box>
-          <Box>
-            <Typography sx={{
-              fontSize: 18,
-              fontWeight: 'bold',
-              letterSpacing: 1
-            }}>
-              Address
-            </Typography>
-            <Typography sx={{
-              fontSize: 18,
-              fontWeight: 'bold',
-              letterSpacing: 1
-            }}>
-              {data.address}
-            </Typography>
+          <Box container component={Grid} justifyContent="space-between" sx={{
+            marginTop: 3,
+            width: 500
+          }}>
+            <Box>
+              <Typography sx={{
+                fontSize: 18,
+                fontWeight: 'bold',
+                letterSpacing: 1
+              }}>
+                Email
+              </Typography>
+              <Typography sx={{
+                fontSize: 18,
+                fontWeight: 'bold',
+                letterSpacing: 1
+              }}>
+                {data.email}
+              </Typography>
+            </Box>
+            <Box>
+              <Typography sx={{
+                fontSize: 18,
+                fontWeight: 'bold',
+                letterSpacing: 1
+              }}>
+                Address
+              </Typography>
+              <Typography sx={{
+                fontSize: 18,
+                fontWeight: 'bold',
+                letterSpacing: 1
+              }}>
+                {data.address}
+              </Typography>
+            </Box>
           </Box>
-        </Box>
-        <Box container component={Grid} justifyContent="space-between" sx={{
-          marginTop: 3,
-          width: 500
-        }}>
-          <Box>
-            <Typography sx={{
-              fontSize: 18,
-              fontWeight: 'bold',
-              letterSpacing: 1
-            }}>
-              Phone
-            </Typography>
-            <Typography sx={{
-              fontSize: 18,
-              fontWeight: 'bold',
-              letterSpacing: 1
-            }}>
-              {data.phone}
-            </Typography>
-          </Box>
-          <Box>
-            <Typography sx={{
-              fontSize: 18,
-              fontWeight: 'bold',
-              letterSpacing: 1
-            }}>
-              Has shop
-            </Typography>
-            <Typography sx={{
-              fontSize: 18,
-              fontWeight: 'bold',
-              letterSpacing: 1,
-              color: data.hasShop === true ? 'green' : 'red'
-            }}>
-              {data.hasShop === true ? 'Yes' : 'No'}
-            </Typography>
+          <Box container component={Grid} justifyContent="space-between" sx={{
+            marginTop: 3,
+            width: 500
+          }}>
+            <Box>
+              <Typography sx={{
+                fontSize: 18,
+                fontWeight: 'bold',
+                letterSpacing: 1
+              }}>
+                Phone
+              </Typography>
+              <Typography sx={{
+                fontSize: 18,
+                fontWeight: 'bold',
+                letterSpacing: 1
+              }}>
+                {data.phone}
+              </Typography>
+            </Box>
+            <Box>
+              <Typography sx={{
+                fontSize: 18,
+                fontWeight: 'bold',
+                letterSpacing: 1
+              }}>
+                Has shop
+              </Typography>
+              <Typography sx={{
+                fontSize: 18,
+                fontWeight: 'bold',
+                letterSpacing: 1,
+                color: data.hasShop === true ? 'green' : 'red'
+              }}>
+                {data.hasShop === true ? 'Yes' : 'No'}
+              </Typography>
+            </Box>
           </Box>
         </Box>
       </Box>
